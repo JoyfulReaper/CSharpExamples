41fa6b207a6f9cca3bc733f46e2b983d17799df8
Kyle W. Givler <xvegan88x@gmail.com>
2020-01-07 17:55:33 -0500

    Rectangular Array (Multi-Dimensional) pg 44

---
+++ Language Basics/RectangularArrays.cs
@@ -0,0 +1,25 @@
+using System;
+
+class RectArray
+{
+    public static void Main()
+    {
+        int [,] matrix = new int[3,3];
+
+        for(int i = 0; i < matrix.GetLength(0); i++)
+            for(int j = 0; j < matrix.GetLength(1); j++)
+                matrix[i,j] = i * 3 + j;
+
+        int[,] matrix2 = new int[,]
+        {
+            {0, 1, 2},
+            {3, 4, 5},
+            {6, 7, 8}
+        };
+
+        for(int i = 0; i < matrix.GetLength(0); i++)
+            for(int j = 0; j < matrix.GetLength(1); j++)
+                Console.WriteLine("matrix[{0},{1}]: {2}", i, j, matrix[i,j]);
+
+    }
+}
