1950992d29ba2849545fb83c0503861317b2fdae
Kyle W. Givler <xvegan88x@gmail.com>
2020-01-07 19:31:49 -0500

    Out Reference Modifier examples

--- Language Basics/OutArgumentModifier.cs
+++ Language Basics/OutArgumentModifier.cs
@@ -14,5 +14,8 @@ class test
         Split("Steve Anita Smith", out a, out b); // Out paramaters are passed by reference
         Console.WriteLine("First Name(s): {0}", a);
         Console.WriteLine("Last Name: {0}", b);
+
+        Split("Stan Something Smith", out string NewA, out _); // Since c# 7 declare variables (NewA on the fly). Ignore an output argument with _
+        Console.WriteLine("First Name(s): {0}", NewA);           
     }
 }
