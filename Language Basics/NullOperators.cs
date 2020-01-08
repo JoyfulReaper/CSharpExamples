ef03f1348ea0727fb052c67de94b7d8df0cafe10
Kyle W. Givler <xvegan88x@gmail.com>
2020-01-07 20:05:27 -0500

    Null Operator Examples

---
+++ Language Basics/NullOperators.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Text;
+
+class Test
+{
+    public static void Main()
+    {
+        // Null Coalescing Operator
+        string s1 = null;
+        string s2 = s1 ?? "s1 is null!";
+        Console.WriteLine(s2);
+
+        s1 = "s1 is not null!";
+        s2 = s1 ?? "s1 ia null!";
+        Console.WriteLine(s2);
+
+        // Null Conditional Operator
+        StringBuilder sb = null;
+        string s3 = sb?.ToString(); // same as: string s3 = (sb == null ? null : sb.ToString());
+        Console.WriteLine(s3 == null ? "s3 is null!" : "s3 is not null!");
+
+        SomeClass sc = null;
+        sc?.someMethod(); // Do nothing / NOOP
+        sc = new SomeClass();
+        sc.someMethod();
+    }
+
+    class SomeClass
+    {
+        public void someMethod () { Console.WriteLine ("someMethod"); }
+    }
+}
