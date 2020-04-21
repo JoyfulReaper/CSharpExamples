50e9bfe9ef6d2c738ae0ca0295e11b78a0e19ee7
Kyle W. Givler <xvegan88x@gmail.com>
2020-04-21 17:52:47 -0400

    Fixed a problem with the variable case preventing a compile and extended the example

--- Creating Types/FieldsAndMethods.cs
+++ Creating Types/FieldsAndMethods.cs
@@ -4,8 +4,8 @@ class Octopus // A class is a reference type
     // Fields: variables that are members of a class or a struct
     string name; // Name field
     public int age = 10; // Optional field initialization
-    static readonly int legs = 8, eyes = 2; // declare two readonly fields at the same time. Readonly fields can not be changed after they are initialized
-    public int HungerPercent {get; private set;} = 0; // Simple propertie
+    static readonly int legs = 8, eyes = 2; // declare two static readonly fields at the same time. Readonly fields can not be changed after they are initialized
+    public int hungerPercent {get; private set;} = 0; // Simple property
 
     // Instance methods
     public bool isHungry()
@@ -13,28 +13,34 @@ class Octopus // A class is a reference type
         return hungerPercent > 60;
     }
     // Method consisting of a single expression can be expressed as an expression-bodied method
-    public void Eat(int amount) => HungerPercent -= amount; 
+    public void Eat(int amount) => hungerPercent -= amount; 
     // Overloaded method (two methods with the same name)
-    public void Eat(string amount) => HungerPercent -= Int32.Parse(amount);
+    public void Eat(string amount) => hungerPercent -= Int32.Parse(amount);
 
-    public void Digest(int amount) => HungerPercent += amount;
+    public void Digest(int amount) => hungerPercent += amount;
 
     public static void Main()
     {
         Octopus o = new Octopus();
-        if (o.isHungry())
-            Console.WriteLine("The octopus is hungry!\n");
-        else
-            Console.WriteLine("The octopus isn't hungry!\n");
+        o.name = "Fred";
         
+        Console.WriteLine("The octopus {0} hungry!\n", o.isHungry() ? "is" : "isn't");
+
+        Console.WriteLine("Digesting 75 food!");
         o.Digest(75);
-        Console.WriteLine("The octopus is {0}% hungry", o.HungerPercent);
+        Console.WriteLine("The octopus is {0}% hungry\n", o.hungerPercent);
+
         Console.WriteLine("Feeding the octopus with an int...");
         o.Eat(10);
-        Console.WriteLine("The octopus is {0}% hungry", o.HungerPercent);
+        Console.WriteLine("The octopus is {0}% hungry\n", o.hungerPercent);
+
         Console.WriteLine("Feeding the octopus with a string...");
         o.Eat("10");
-        Console.WriteLine("The octopus is {0}% hungry", o.HungerPercent);
+        Console.WriteLine("The octopus is {0}% hungry\n", o.hungerPercent);
+
+        Console.WriteLine("The octopus is named: {0}", o.name);
+        Console.WriteLine("The octopus is age: {0}", o.age);
+        Console.WriteLine("The octopus has {0} legs and {1} eyes!", Octopus.legs, Octopus.eyes);
     }
 }
 
