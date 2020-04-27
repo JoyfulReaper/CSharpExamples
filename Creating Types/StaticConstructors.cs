f75a3102a1740856000a41c9f5adbd1042906552
Kyle W. Givler <xvegan88x@gmail.com>
2020-04-27 14:47:00 -0400

    Static class/static constructor examples

--- Creating Types/StaticConstructors.cs
+++ Creating Types/StaticConstructors.cs
@@ -7,12 +7,17 @@ class Test
 {
     public static int a {get; set;}
 
-    static Test() => Console.WriteLine("In static constructor");
+    static Test() 
+    {
+        Console.WriteLine("In static constructor");
+        a = 100;
+    }
     public Test() => Console.WriteLine("In Constuctor");
 
     public static void Main()
     {
         Console.WriteLine("Creating a test object");
         Test t = new Test();
+        Console.WriteLine(a);
     }
 }
