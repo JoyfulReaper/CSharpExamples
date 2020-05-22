06c8297a46be6a89bb85500b4f8ded43b5b8ecf8
Kyle Givler <xvegan88x@gmail.com>
2020-05-22 14:13:18 -0400

    Anonymous Types Example

---
+++ Advanced C#/AnonymouseTypes.cs
@@ -0,0 +1,21 @@
+using System;
+class Program
+{
+    public static void Main()
+    {
+        var Person = new { Name = "Sam", Age = 33 }; // Anonymous Type
+
+        int age = 23;
+        var Person2 = new { Name = "Sally", age, age.ToString().Length }; // Property name is inffered
+        Console.WriteLine($"Name: {Person2.Name} Age: {Person2.age} Length: {Person2.Length}");
+
+        var People = new[] // Array of anonymous types
+        {
+            new { Name = "Bob", Age = 12 },
+            new { Name = "Bertha", Age = 82 },
+        };
+
+        dynamic Foo() => new {Name = "Bob", Age = 30}; // Not static type saftey
+        Console.WriteLine(Foo());
+    }
+}
