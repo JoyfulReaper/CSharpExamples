9d389bba99cb500a0addcb4c2c685637812ab5ec
Kyle Givler <xvegan88x@gmail.com>
2020-11-14 10:26:01 -0500

    Exstension Method Example

--- Advanced C#/ExtensionMethods.cs
+++ Advanced C#/ExtensionMethods.cs
@@ -4,6 +4,7 @@
 
 
 using System;
+using System.Text;
 
 // Static method in a static class
 public static class StringHelper
@@ -11,8 +12,53 @@ public static class StringHelper
     public static bool IsCapitalized (this string s) // this modifer applied to fisrt parameter, the type to be extended
     {
         if(string.IsNullOrEmpty(s))
+        {
             return false;
+        }
+
         return char.IsUpper(s[0]);
+    }
+
+    public static bool EndsWithPeriod(this string s)
+    {
+        if(string.IsNullOrEmpty(s))
+        {
+            return false;
+        }
+
+        return s.EndsWith(".");
+    }
+
+    public static string Pluralize(this string s)
+    {
+        if(string.IsNullOrEmpty(s))
+        {
+            return "";
+        }
+
+        if(!s.EndsWith("s"))
+        {
+            s = s + "s";
+        }
+
+        return s;
+    }
+
+    public static string Capitalize(this string s)
+    {
+        if(string.IsNullOrEmpty(s))
+        {
+            return "";
+        }
+
+        StringBuilder sb = new StringBuilder(s);
+
+        if(!Char.IsUpper(sb[0]))
+        {
+            sb[0] = char.ToUpper(sb[0]);
+        }
+
+        return sb.ToString();
     }
 }
 
@@ -21,9 +67,15 @@ public class Program
     public static void Main()
     {
         Console.WriteLine("Enter a sentence:");
-        string sentense = Console.ReadLine();
+        string sentence = Console.ReadLine();
 
         // Can be called as if it was an instance method
-        Console.WriteLine("Your sentence {0} start with a capital letter.", sentense.IsCapitalized() ? "does" : "does not");
+        Console.WriteLine("Your sentence {0} start with a capital letter.", sentence.IsCapitalized() ? "does" : "does not");
+        Console.WriteLine("Your sentence {0} end with a period.", sentence.EndsWithPeriod() ? "does" : "does not");
+        Console.WriteLine();
+
+        Console.WriteLine("Enter a word:");
+        string word = Console.ReadLine();
+        Console.WriteLine(word.Capitalize().Pluralize()); // Exstenion methon chaining
     }
 }
